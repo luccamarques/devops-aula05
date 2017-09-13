f630accc382e4133920dbd3cdd721a272a2a7313
luccamarques <30813613+luccamarques@users.noreply.github.com>
2017-09-13 16:33:40 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -4,10 +4,10 @@ for i in range(3):
 linha = [ ]
 for j in range(3):
 linha.append(".")
-tab.append(x)
+tab.append(linha)
 return tab
 def main( ):
 jogo = inicializar( )
 print (jogo)
 if __name__ == "__main__":
-main()
+main'
